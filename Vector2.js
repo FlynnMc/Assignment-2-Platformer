90077d745ae376af7d5d01eaba80daf76dc1af77
FlynnMc <620mccoflyn@rokstu.catholic.edu.au>
2015-08-26 20:51:44 +1000

    Pretty Sweet
    Ass

---
+++ Vector2.js
@@ -0,0 +1,17 @@
+var Vector2 = function()
+{
+	this.x;
+	this.y;
+};
+
+Vector2.prototype.SetValues = function()
+{
+	this.x = 0;
+	this.y = 0;
+};
+
+Vector2.prototype.normalise=function(x,y)
+{
+	
+}
+
